f366b962cae92164ef0fcd1edd4d4c199208e082
Magic-shortbow <llombluc@gmail.com>
2026-02-24 15:25:04 +0000

    init privacy policy

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,11 @@
+export default function Privacy() {
+  return (
+    <div className="w-full h-screen bg-slate-50">
+      <iframe
+        src="/privacy_policy.html"
+        className="w-full h-full border-none"
+        title="Privacy Policy"
+      />
+    </div>
+  );
+}
