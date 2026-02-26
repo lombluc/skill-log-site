10013bcc680c276e4e09f059993c59dc8fa9478a
Magic-shortbow <llombluc@gmail.com>
2026-02-26 16:00:49 +0000

    cookie policy

---
+++ src/app/cookies/page.tsx
@@ -0,0 +1,11 @@
+export default function Cookies() {
+  return (
+    <div className="w-full h-screen bg-slate-50">
+      <iframe
+        src="/cookie_policy.html"
+        className="w-full h-full border-none"
+        title="Cookie Policy"
+      />
+    </div>
+  );
+}
